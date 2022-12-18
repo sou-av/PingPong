5cebb6418d3705d4428c5a62eb214e6ab3d90f62
av <antonio.vinicius.sousa07@aluno.ifce.edu.br>
2022-12-18 15:03:09 +0000

    Primeiro commit.
    Janela iniciada
    Bola acompanha mouse
    Detecta clicks e presses

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author Alunos
+ *
+ */
+module pong {
+	requires java.desktop;
+}
---
+++ src/pong/Main.java
@@ -0,0 +1,13 @@
+package pong;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		new Display("Ping Pong", 500, 500);
+	}
+
+}
