c3a1a1744e04dd4d849a1e3fd130cc1f652c4c01
sou-av <antonio.vinicius.sousa07@aluno.ifce.edu.br>
2022-12-19 15:27:51 -0300

    Integrando barras no codigo Main

--- src/pong/Main.java
+++ src/pong/Main.java
@@ -6,14 +6,26 @@ public class Main {
 	Display display = new Display("Ping Pong", 500, 500);
 	Ball ball = new Ball(15);
 	BallController ballController;
+	BarController barController;
+	Bar bar1 = new Bar(20, 80);
+	Bar bar2 = new Bar(20, 80);
 
 	Main() {
+		bar1.setPos(20, 250);
+		bar2.setPos(450, 250);
 		ballController = new BallController(this);
+		barController = new BarController(this);
+		
+		display.canvas.addKeyListener(barController);		
+		
 		display.children.add(ball);
+		display.children.add(bar1);
+		display.children.add(bar2);
 	}
 
 	public static void main(String[] args) {
 		Main game = new Main();
+		game.barController.start();
 		game.ballController.run();
 	}
 
