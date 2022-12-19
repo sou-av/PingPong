2ff60fd6476ac7b332948b493758b65c7ce04dd1
av <antonio.vinicius.sousa07@aluno.ifce.edu.br>
2022-12-19 12:16:09 +0000

    Isolando Display
    adicionado children que contem os componentes a serem desenhados

--- src/pong/Display.java
+++ src/pong/Display.java
@@ -3,14 +3,15 @@ package pong;
 import javax.swing.JFrame;
 import java.awt.Canvas;
 import java.awt.Graphics;
-import java.awt.event.MouseMotionListener;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseEvent;
 import java.awt.Color;
+import java.util.List;
+import java.util.LinkedList;
 
-public class Display {
+public class Display implements Drawlable {
 	JFrame window;
 	Canvas canvas;
+	List<Drawlable> children = new LinkedList<>();
+
 
 	public Display(String title, int width, int height) {
 		window = new JFrame(title);
@@ -18,6 +19,20 @@ public class Display {
 		window.add(canvas);
 		window.setSize(width, height);
 		window.setVisible(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+
+	@Override
+	public void draw(Graphics g) {
+		for (Drawlable child : children)
+			child.draw(g);
+	}
+
+
+	public void clear(Graphics g) {
+		g.setColor(canvas.getBackground());
+		g.fillRect(0,0, canvas.getWidth(), canvas.getHeight());
 	}
 
 }
