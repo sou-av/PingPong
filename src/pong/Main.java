c084ea3a33ec6430a2477dee4852d14b9c181402
sou-av <antonio.vinicius.sousa07@aluno.ifce.edu.br>
2022-12-21 18:33:03 +0000

    Alteracoes na Main
    adicionado placar
    BarController escuta janela (evita bug do foco)
    alterando x de bar1 em setPos para 15

--- src/pong/Main.java
+++ src/pong/Main.java
@@ -5,22 +5,26 @@ import java.awt.Graphics;
 public class Main {
 	Display display = new Display("Ping Pong", 500, 500);
 	Ball ball = new Ball(15);
-	BallController ballController;
-	BarController barController;
 	Bar bar1 = new Bar(20, 80);
 	Bar bar2 = new Bar(20, 80);
+	BallController ballController;
+	BarController barController;
+	Score score;
 
 	Main() {
-		bar1.setPos(20, 250);
+		bar1.setPos(15, 250);
 		bar2.setPos(450, 250);
+		
 		ballController = new BallController(this);
 		barController = new BarController(this);
+		score = new Score(this);
 		
-		display.canvas.addKeyListener(barController);		
+		display.window.addKeyListener(barController);		
 		
 		display.children.add(ball);
 		display.children.add(bar1);
 		display.children.add(bar2);
+		display.children.add(score);
 	}
 
 	public static void main(String[] args) {
